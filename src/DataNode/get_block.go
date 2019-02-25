3b7d1bc5a92ca150edbbace75ead5c91c2cb4933
stevphan <stevenphan97@gmail.com>
2019-02-25 14:44:25 -0800

    added files for DataNode API Calls

---
+++ src/DataNode/get_block.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"log"
+)
+
+func getBlock(args []string) { // returns block requested
+	/*
+	0: Block ID
+
+	// TODO: change argument type to valid type (type BlockID)
+	*/
+}
---
+++ src/DataNode/store_and_foward.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"log"
+)
+
+func StoreAndForward(args []string) { // 
+	/*
+	0: Block
+	1: BlockID
+	2: DataNodeList
+	*/
+
+	// TODO: change arguments to fit correct type
+	// TODO: check for valid filename
+	// TODO: check for valid save location
+
+}
