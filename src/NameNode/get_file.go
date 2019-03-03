02109094eefc6563e9ede5405cb5ef8a2b789a6a
unknown <grantludwig97@gmail.com>
2019-03-02 23:10:34 -0800

    Create file works? get file in progress

--- src/NameNode/get_file.go
+++ src/NameNode/get_file.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/json"
+	"log"
 	"net/http"
 )
 
@@ -8,5 +10,34 @@ const (
 )
 
 func getFile(write http.ResponseWriter, req *http.Request) { //return dataNode list per block
-	
+	decoder := json.NewDecoder(req.Body)
+	myReq := createRequest{}
+	err := decoder.Decode(&myReq)
+	log.Println(err)
+
+	found := false
+	if files.numFiles < 1 {
+		//TODO fail write (DONE?)
+		myRes := responseObj{}
+		js, err := convertObjectToJson(myRes)
+		log.Print(err)
+		write.Header().Set("Content-Type", "application/json")
+		write.Write(js)
+		return
+	} else {
+		for i := 0; i < files.numFiles; i++ {
+			if files.metaData[i].fileName == myReq.Filename {
+				found = true
+			}
+		}
+	}
+
+	if !found {
+		myRes := responseObj{}
+		js, err := convertObjectToJson(myRes)
+		log.Print(err)
+		write.Header().Set("Content-Type", "application/json")
+		write.Write(js)
+		return
+	}
 }
--- src/NameNode/main.go
+++ src/NameNode/main.go
@@ -1,14 +1,16 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"net/http"
 )
 
 const (
-	RepFact = 3
+	repFact = 3
 )
 
-type files struct {
+type file struct {
 	numFiles int
 	metaData []fileMetaData
 }
@@ -20,20 +22,51 @@ type fileMetaData struct {
 }
 
 type blockList struct {
-	DnList [RepFact]string
+	dnList [repFact]string //stores IP of DNs it is stored at
 }
 
 var (
-	DnList = []string{}
-	NumDn int
+	dnList = []string{}
+	numDn = 0
+	files file
 )
 
 func main() {
 	//TODO read from disk, build the files from that
+	//for testing
+	dnList = append(dnList, "Hello")
+	numDn++
+	dnList = append(dnList, "Hello1")
+	numDn++
+	dnList = append(dnList, "Hello2")
+	numDn++
+	dnList = append(dnList, "Hello3")
+	numDn++
+
 	http.HandleFunc("/create_file", createFile)
 	http.HandleFunc("/get_file", getFile)
 	http.HandleFunc("/block_report", blockReport)
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		panic(err)
 	}
+}
+
+func convertObjectToJsonBuffer(object interface{}) (*bytes.Buffer, error) {
+	data, err := json.Marshal(object)
+	if err != nil {
+		return nil, err
+	}
+
+	buffer := bytes.NewBuffer(data)
+
+	return buffer, nil
+}
+
+func convertObjectToJson(object interface{}) ([]byte, error) {
+	data, err := json.Marshal(object)
+	if err != nil {
+		return nil, err
+	}
+
+	return data, nil
 }
