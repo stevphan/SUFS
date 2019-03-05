a67906757a0f23c3b63ec3ed95443400f6724e19
unknown <grantludwig97@gmail.com>
2019-03-04 20:53:50 -0800

    more work in block reports

--- src/NameNode/block_report.go
+++ src/NameNode/block_report.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
+	"strconv"
+	"strings"
 )
 
 const (
@@ -30,7 +33,42 @@ func blockReport(write http.ResponseWriter, req *http.Request) { //returns nothi
 		numDn++
 	}
 
+	var ipFound bool
 	for i := 0; i < len(myReq.BlockId); i++ {
-
+		ipFound = false
+		temp := strings.Split(myReq.BlockId[i], "_")
+		fileName, blockNum := temp[0], temp[1]
+		blockId, err := strconv.Atoi(blockNum)
+		errorPrint(err)
+		found, fileIndex := findFile(fileName)
+		if found {
+			for j := 0; j < repFact; j++ {
+				if myReq.MyIp == files.MetaData[fileIndex].BlockLists[blockId].DnList[j]{
+					ipFound = true
+				}
+			}
+			if !ipFound {
+				j := 0
+				for !ipFound || j < repFact{
+					if files.MetaData[fileIndex].BlockLists[blockId].DnList[j] == "" {
+						files.MetaData[fileIndex].BlockLists[blockId].DnList[j] = myReq.MyIp
+					}
+				}
+			}
+		}
 	}
+	fmt.Println()
+}
+
+//Returns true if file name found plus the index it is in the files.metadata[]
+//Returns false if file name not found plus index of -1
+func findFile(fileName string) (found bool, fileIndex int) {
+	if files.NumFiles > 0 {
+		for i := 0; i < files.NumFiles; i++ {
+			if files.MetaData[i].FileName == fileName {
+				return true, i
+			}
+		}
+	}
+	return false, -1
 }
