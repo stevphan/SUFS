0ea30ad34198caed62824d11b21d86285c0f192f
unknown <grantludwig97@gmail.com>
2019-03-04 17:55:44 -0800

    block report started

--- src/NameNode/block_report.go
+++ src/NameNode/block_report.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
 )
 
@@ -8,5 +9,28 @@ const (
 )
 
 func blockReport(write http.ResponseWriter, req *http.Request) { //returns nothing, this is what happens when a block report is received
-	
+	decoder := json.NewDecoder(req.Body)
+	myReq := blockReportRequest{}
+	err := decoder.Decode(&myReq)
+	errorPrint(err)
+
+	found := false
+	if numDn < 1 {
+		dnList = append(dnList, myReq.MyIp)
+		numDn++
+	} else {
+		for i := 0; i < numDn; i++ {
+			if dnList[i] == myReq.MyIp {
+				found = true
+			}
+		}
+	}
+	if !found {
+		dnList = append(dnList, myReq.MyIp)
+		numDn++
+	}
+
+	for i := 0; i < len(myReq.BlockId); i++ {
+
+	}
 }
