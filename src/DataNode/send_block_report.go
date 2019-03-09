20373f37da19c5f68de68000679464c4e9a65cd1
stevphan <stevenphan97@gmail.com>
2019-03-09 02:14:03 -0800

    completed (?) heartbeat sending, blockreport sending, and replicate

---
+++ src/DataNode/send_block_report.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"io/ioutil"
+	"log"
+	"net/http"
+	"shared"
+	"time"
+)
+
+const (
+	blockReportTimer = 60 //in seconds
+)
+
+func send_block_report() { // sends a block report every blockReportTimer seconds, so every minute
+	ticker := time.NewTicker(blockReportTimer * time.Second)
+	for range ticker.C {
+		log.Println("sending block report")
+		create_report()
+	}
+}
+
+func create_report() {
+	myBlockReport := shared.BlockReportRequest{MyIp:selfAddress}
+	blocks, err := ioutil.ReadDir(directory)
+	nameNodeUrl := "http://" + nameNodeAddress + "/blockReport"
+	if err != nil {
+		log.Fatal(err)
+	}
+	// grab blocks in directory
+	for _, b := range blocks {
+		myBlockReport.BlockIds = append(myBlockReport.BlockIds, b.Name())
+	}
+	// send as POST to NN, since block report we don't needs a response
+	buffer, err := shared.ConvertObjectToJsonBuffer(myBlockReport)
+	_, _ = http.Post(nameNodeUrl,"application/json", buffer)
+}
+
---
+++ src/DataNode/send_heartbeat.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"shared"
+	"time"
+)
+
+const (
+	heartTimer = 10 //in ms
+)
+
+
+func send_heartbeat() { // sends heartbeat every heartTimer amount of seconds, so every 10 seconds
+	ticker := time.NewTicker(heartTimer * time.Second)
+	for range ticker.C {
+		log.Println("sending heartbeat")
+		heartbeat()
+	}
+}
+
+func heartbeat() {
+	nameNodeUrl := "http://" + nameNodeAddress + "/heartBeat"
+	heartbeatReq := shared.HeartbeatRequest{}
+	heartbeatReq.MyIp = selfAddress
+	buffer, err := shared.ConvertObjectToJsonBuffer(heartbeatReq)
+	res, err := http.Post(nameNodeUrl, "application/json", buffer)
+	// should I even keep response if heartbeat is one-way anyway?
+	heartbeatResp := shared.HeartbeatResponse{}
+	err = shared.ObjectFromResponse(res, &heartbeatResp)
+	shared.CheckErrorAndFatal("Error sending heartbeat", err)
+}
